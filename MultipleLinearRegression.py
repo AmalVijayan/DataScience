68c02bf265671b55b5bb21607ca696acccaee4ae
AmalVijayan <amalvnair95@gmail.com>
2018-10-28 21:08:43 +0530

    Committing Python program for Multiple Linear Regression

---
+++ MultipleLinearRegression.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 28 11:32:07 2018
+
+@author: amal
+"""
+
+########################### Data Preprocessing ###################################
+
+#1 Importing the libraries
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#2 importing the data set
+
+dataset = pd.read_csv('50_Startups.csv')
+
+
+#3 classifying dependent and independent variables
+
+X = dataset.iloc[:,:-1].values
+Y = dataset.iloc[:,-1].values
+
+"""
+#4 dealing with missing values
+
+from sklearn.preprocessing import Imputer
+imp = Imputer(missing_values="NaN", strategy="mean", axis=0)
+X[:,:-1] = imp.fit_transform(X[:,:-1])
+"""
+
+
+#5 dealing with categorical data
+
+from sklearn.preprocessing import LabelEncoder,OneHotEncoder
+label_X = LabelEncoder()
+X[:,-1] = label_X.fit_transform(X[:,-1])
+ohe_X = OneHotEncoder(categorical_features = [3])
+X = ohe_X.fit_transform(X).toarray()
+
+"""Avoiding the Dummy variable trap """
+X = X[:,1:]  # some libraries require it to be taken care manualy
+
+#6 Splitting into training and testing set
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y,test_size = 0.2,random_state = 0)
+
+"""
+#7 Feature Scaling
+
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.fit(X_test)
+
+"""
+
+
+#################Data Preprocessing Ends #################################
+
+
+
+""" Multiple Linear regression """
+
+#1 Creating the Regressor machine and providing the training sets
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train,Y_train)
+
+
+#2 Predicting the result for the test set
+
+Y_pred = regressor.predict(X_test)
+
+
+#3 performing backward elimination to find X_opt
+import statsmodels.formula.api as sm
+# add a column of ones to match the regression formulae
+X = np.append(arr = np.ones(shape = (50,1)).astype(int),values = X, axis = 1)
+
+X_opt = X[:, [0,1,2,3,4,5]]
+regressor_ols = sm.OLS(endog=Y, exog=X_opt).fit()
+regressor_ols.summary()
+
+#removing X[] with greatest p_value 
+
+X_opt = X[:, [0,1,3,4,5]]
+regressor_ols = sm.OLS(endog=Y, exog=X_opt).fit()
+regressor_ols.summary()
+
+#removing X[] with greatest p_value
+
+X_opt = X[:, [0,3,4,5]]
+regressor_ols = sm.OLS(endog=Y, exog=X_opt).fit()
+regressor_ols.summary()
+
+#removing X[] with greatest p_value
+
+X_opt = X[:, [0,3,5]]
+regressor_ols = sm.OLS(endog=Y, exog=X_opt).fit()
+regressor_ols.summary()
+
+#removing X[] with greatest p_value
+
+X_opt = X[:, [0,3]]
+regressor_ols = sm.OLS(endog=Y, exog=X_opt).fit()
+regressor_ols.summary()
+
+
